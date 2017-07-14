8c6af7a0aa68f052a66c2cc8eb48717cf4cf233d
Ethan <konmyn@163.com>
2017-07-14 16:40:53 +0800

    company coding

---
+++ class_attr.py
@@ -0,0 +1,11 @@
+# -*- coding:utf8 -*-
+
+class HD(object):
+    user = 'jerry'
+    def __init__(self, user):
+        self.user = user
+
+if __name__ == '__main__':
+    a = HD('tom')
+    print a.user
+    print a.__class__.user
---
+++ requests_auth.py
@@ -0,0 +1,9 @@
+# -*- coding:utf8 -*-
+
+import requests
+from requests.auth import HTTPBasicAuth
+user = 'guest'
+password='guest'
+headers = {'content-type': 'application/json'}
+url = 'http://182.148.108.30:8183/hdcard-services/api/member/query_by_mobilenum?mobilenum=15502111710'
+r = requests.get(url, headers=headers, auth=HTTPBasicAuth(user, password))
