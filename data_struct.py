20d0545b5799cd4fa27b53500c6d8e58e2f7de9b
Ethan <konmyn@163.com>
2017-09-27 21:18:05 +0800

    add data struct

---
+++ data_struct.py
@@ -0,0 +1,30 @@
+# -*- coding: utf8 -*-
+
+# 标签所有的列集合
+columns = {
+    "name": "name",
+    "order": "order",
+    "column": ["column0", "column1", "column2"],
+}
+
+# 单个列所有的组集合
+column = {
+    "name": "name",
+    "order": "order",
+    "groups": ["group0", "group1", "group2"],
+}
+
+# 单个组所有的链接集合
+group0 = {
+    "name": "name",
+    "order": "order",
+    "links": ["link0", "link1", "link2"],
+}
+
+# 单个链接所有的属性集合
+link0 = {
+    "name": "name",
+    "link": "link",
+    "order": "order",
+    "favicon": "favicon",
+}
