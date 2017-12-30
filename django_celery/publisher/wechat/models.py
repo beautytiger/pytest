19b1aa5b1c2d27e9e21b276f0173781c4163071e
Ethan <konmyn@163.com>
2017-12-30 22:15:05 +0800

    working on get wechat user info, not done yet

--- django_celery/publisher/wechat/models.py
+++ django_celery/publisher/wechat/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 import time
 import requests
+import json
 from collections import namedtuple
 
 from django.db import models
@@ -69,9 +70,10 @@ class WeChator(CommonBaseModel):
 
     # https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421140839
     WECHAT_USER_INFO = "https://api.weixin.qq.com/cgi-bin/user/info?access_token={access_token}&openid={openid}&lang=zh_CN"
+    BATCH_INFO = "https://api.weixin.qq.com/cgi-bin/user/info/batchget?access_token={access_token}"
 
     # the user using wechat
-    subscribe = models.BooleanField(verbose_name="is user subscribed the app")
+    subscribe = models.BooleanField(default=False, verbose_name="is user subscribed the app")
     appid = models.CharField(max_length=64, null=False, db_index=True, verbose_name="wechat appid")
     openid = models.CharField(max_length=128, null=False, db_index=True, verbose_name="wechator openid")
     nickname = models.CharField(max_length=128, default="", verbose_name="wechator nickname")
@@ -85,7 +87,7 @@ class WeChator(CommonBaseModel):
     unionid = models.CharField(max_length=128, default="", db_index=True, verbose_name="union id for wechator")
     app_remark = models.CharField(max_length=64, default="", verbose_name="app owner remark to wechator")
     groupid = models.IntegerField(default=0, verbose_name="wechat group id")
-    tagid_list = models.TextField(default="", verbose_name="tag id to this user")
+    tagid_list = models.TextField(default="[]", verbose_name="tag id to this user")
 
     class Meta:
         unique_together = (("appid", "openid"), )
@@ -98,4 +100,51 @@ class WeChator(CommonBaseModel):
         if "errcode" in result:
             return RESULT(result["errcode"], result["errmsg"], WSC.get(result["errcode"], u"未知错误"))
         else:
-            instance = cls.objects.create()
+            created, instance = cls.objects.create_or_get(appid=appid, openid=openid)
+            if result.get("subscribe") == 1:
+                instance.subscribe = True
+                instance.nickname = result["nickname"]
+                instance.sex = result["sex"]
+                instance.language = result["language"]
+                instance.city = result["city"]
+                instance.country = result["country"]
+                instance.province = result["province"]
+                instance.headimgurl = result["headimgurl"]
+                instance.last_subscribe_time = result["subscribe_time"]
+                instance.unionid = result.get("unionid", "")
+                instance.app_remark = result["remark"]
+                instance.groupid = result.get("groupid", "")
+                # below should be be json object
+                instance.tagid_list = json.dumps(result["tagid_list"], "[]")
+            else:
+                instance.subscribe = False
+            instance.save()
+            return RESULT(0, instance, u"请求成功")
+
+    @classmethod
+    def batch_update_wechator_info(cls):
+        for i in cls.objects.values("appid").annotate(count=models.Count("appid")):
+            appid = i["appid"]
+            access_token = WeChatApp.objects.get(appid=appid).get_access_token()
+            data = {"user_list": []}
+            for j in cls.objects.filter(appid=appid).values("appid", "language"):
+                data["user_list"].append(j)
+                if len(data["user_list"] < 100):
+                    continue
+                else:
+                    cls._batch_update(appid, access_token, data)
+                    data = {"user_list": []}
+            if data["user_list"] != []:
+                cls._batch_update(appid, access_token, data)
+
+    @classmethod
+    def _batch_update(cls, appid=None, access_token=None, data=None):
+        url = cls.BATCH_INFO.format(access_token=access_token)
+        response = requests.post(url, data=data)
+        result = response.json()
+        if "error_code" in result:
+            return False
+        else:
+            for user in result["user_info_list"]:
+                pass
+            return True
